eeff273291e9fbcca69b309e94e45fa73fe15c74
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2026-04-01 22:48:28 -0600

    Run both apps together

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+
+// to start both application at once
+concurrently([
+  {
+    name: "server",
+    command: "bun run dev",
+    cwd: "packages/server",
+    prefixColor: "cyan",
+  },
+  {
+    name: "client",
+    command: "bun run dev",
+    cwd: "packages/client",
+    prefixColor: "green",
+  },
+]);
