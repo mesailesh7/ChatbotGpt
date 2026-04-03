23decfd420f8424c3b43f98b3690ddf39c56417f
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2026-04-02 20:49:31 -0600

    UI code refactored

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,13 +1,15 @@
-import ChatBot from "./components/ChatBot"
+import ChatBot from './components/ChatBot';
 
 function App() {
 
   return (
     <>
-      <ChatBot />
+      <div className="p-4 h-screen w-full ">
+        <ChatBot />
+      </div>
     </>
 
-  )
+  );
 }
 
-export default App
+export default App;
--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -4,6 +4,7 @@ import ReactMarkdown from 'react-markdown';
 import { FaArrowUp } from 'react-icons/fa';
 import { Button } from './ui/button';
 import axios from 'axios';
+import * as React from 'react';
 
 type FormData = {
   prompt: string;
@@ -21,6 +22,7 @@ type Message = {
 export default function ChatBot() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [isBotTyping, setIsBotTyping] = useState(false);
+  const lastMessageRef = useRef<HTMLDivElement | null>(null);
   const conversationId = useRef(crypto.randomUUID());
   const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
@@ -28,7 +30,7 @@ export default function ChatBot() {
     setMessages(prev => [...prev, { content: prompt, role: 'user' }]);
     setIsBotTyping(true);
 
-    reset();
+    reset({ prompt: '' });
     const { data } = await axios.post<ChatResponse>('/api/chat', {
       prompt,
       conversationId: conversationId.current,
@@ -44,25 +46,31 @@ export default function ChatBot() {
     }
   };
 
+  const onCopyMessage = (e: React.ClipboardEvent<HTMLParagraphElement>) => {
+    const selection = window.getSelection()?.toString().trim();
+    if (selection) {
+      e.preventDefault();
+      e.clipboardData.setData('text/plain', selection);
+    }
+  };
+
   return (
-    <div>
-      <div className="flex flex-col gap-3 mb-10">
-        {messages.map((message, index) => (
-          <p key={index}
-             onCopy={e => {
-               const selection = window.getSelection()?.toString().trim();
-               if (selection) {
-                 e.preventDefault();
-                 e.clipboardData.setData('text/plain', selection);
-               }
-             }}
-             className={` rounded-xl px-3 py-1 ${message.role === 'user'
-               ? 'bg-blue-600 text-white self-end'
-               : 'bg-gray-100 text-black self-start'}`}
-          >
-            <ReactMarkdown>{message.content}</ReactMarkdown>
-          </p>
-        ))}
+    <div className="flex flex-col h-full">
+      <div className="flex flex-col flex-1 gap-3 mb-10 overflow-y-auto">
+        {messages.map((message, index) => {
+
+          return (
+            <div key={index}
+                 onCopy={onCopyMessage}
+                 ref={index === messages.length - 1 ? lastMessageRef : null}
+                 className={` rounded-xl px-3 py-1 ${message.role === 'user'
+                   ? 'bg-blue-600 text-white self-end'
+                   : 'bg-gray-100 text-black self-start'}`}
+            >
+              <ReactMarkdown>{message.content}</ReactMarkdown>
+            </div>
+          );
+        })}
         {isBotTyping && (
           <div className="flex self-start gap-1 px-3 py-3 bg-gray-200 rounded-xl ">
             <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse" />
@@ -80,6 +88,7 @@ export default function ChatBot() {
         className="flex flex-col gap-2 items-end border-2 rounded-3xl">
           <textarea
             {...register('prompt', { required: true, validate: (data) => data.trim().length > 0 })}
+            autoFocus
             className="w-full border-0 focus:outline-0 resize-none " placeholder="Ask Anything" maxLength={1000} />
         <Button
           disabled={!formState.isValid}
